9935d8e11678f76c3d793c09e5c8fb0e34f36bac
Artem <mart_22@ukr.net>
2023-01-30 13:38:50 +0200

    Header nodes position in blockPosition.js

--- src/js/modules/blockPosition.js
+++ src/js/modules/blockPosition.js
@@ -1,5 +1,13 @@
 // BLOCK POSITION
 export default function blockPosition() {
+	const bl1 = document.querySelector('.header__bl-2 > div:first-child'),
+		bl2 = document.querySelector('.header__add > div:first-child'),
+		bl3 = document.querySelector('.header__add > div:last-child'),
+		bl4 = document.querySelector('.toggle__content > div'),
+		bl5 = document.querySelector('.header__nav'),
+		bl6 = document.querySelector('.header__btn'),
+		bl7 = document.querySelector('.header__lang');
+
 	if (window.matchMedia('(min-width: 361px)').matches) {
 		document.body.classList.remove('max-360');
 
@@ -51,6 +59,15 @@ export default function blockPosition() {
 			document.body.classList.add('min-1201');
 
 			if (document.querySelector('.min-1201')) {
+				if (bl1 && bl5) {
+					bl1.prepend(bl5);
+				}
+				if (bl2 && bl6) {
+					bl2.prepend(bl6);
+				}
+				if (bl7 && bl7) {
+					bl3.prepend(bl7);
+				}
 			}
 		}
 	}
@@ -62,6 +79,15 @@ export default function blockPosition() {
 			document.body.classList.add('max-1200');
 
 			if (document.querySelector('.max-1200')) {
+				if (bl4 && bl7) {
+					bl4.prepend(bl7);
+				}
+				if (bl5 && bl7) {
+					bl7.after(bl5);
+				}
+				if (bl4 && bl6) {
+					bl4.append(bl6);
+				}
 			}
 		}
 	}
